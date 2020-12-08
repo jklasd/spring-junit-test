c9575c9754fee26781f4ba748e4093e5f94f33ce
jubin.zhang <jub5@sina.com>
2020-12-08 16:59:16 +0800

    TestUtil.test = true;

--- src/main/java/com/junit/test/TestUtil.java
+++ src/main/java/com/junit/test/TestUtil.java
@@ -34,6 +34,9 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class TestUtil implements ApplicationContextAware{
 	public static boolean test;
+	static {
+		test = true;
+	}
 	public TestUtil() {
 		log.info("实例化TestUtil");
 	}
