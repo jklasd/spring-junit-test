6e77f4243f6112e847c876e133e64616570bc76a
jubin.zhang <jub5@sina.com>
2021-07-22 08:58:41 +0800

    抽象类代理

--- src/main/java/com/github/jklasd/test/spring/suppert/AopContextSuppert.java
+++ src/main/java/com/github/jklasd/test/spring/suppert/AopContextSuppert.java
@@ -3,8 +3,11 @@ package com.github.jklasd.test.spring.suppert;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 import org.springframework.aop.framework.AopContext;
+
+import com.github.jklasd.test.lazybean.beanfactory.LazyBean;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,19 +17,25 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class AopContextSuppert{
-    private static AopContext aopContext;
+    private static Object aopContext;
     private static Method setCurrentProxy;
     public static void setProxyObj(Object obj) {
         try {
             if (aopContext == null) {
-            	/**
-            	 * 兼容AopContext 升级处理
-            	 */
-            	Constructor<?>[] cons = AopContext.class.getDeclaredConstructors();
-            	if(!cons[0].isAccessible()) {
-            		cons[0].setAccessible(true);
-            	}
-            	aopContext = (AopContext) cons[0].newInstance();
+                /**
+                 * 兼容AopContext 升级处理
+                 */
+                if(Modifier.isAbstract(AopContext.class.getModifiers())) {
+                    //抽象类处理
+                    aopContext = LazyBean.getInstance().buildProxy(AopContext.class);
+                }else {
+                    //非抽象类处理
+                    Constructor<?>[] cons = AopContext.class.getDeclaredConstructors();
+                    if(!cons[0].isAccessible()) {
+                        cons[0].setAccessible(true);
+                    }
+                    aopContext = cons[0].newInstance();
+                }
                 setCurrentProxy = AopContext.class.getDeclaredMethod("setCurrentProxy", Object.class);
                 if(!setCurrentProxy.isAccessible()) {
                     setCurrentProxy.setAccessible(true);
