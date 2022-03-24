54f8a6965ecd56824472143d0a096607da7e62e4
jubin.zhang <jub5@sina.com>
2022-03-24 22:41:17 +0800

    boot-2.4

--- spring-junit-test-spring-boot-1.5/src/main/java/com/github/jklasd/test/lazyplugn/LazyPlugnBeanFactory.java
+++
@@ -1,7 +0,0 @@
- package com.github.jklasd.test.lazyplugn;
-
-import com.github.jklasd.test.lazybean.beanfactory.AbstractLazyProxy;
-
-public interface LazyPlugnBeanFactory {
-     Object buildBean(AbstractLazyProxy model);
-}
--- spring-junit-test-spring-boot-1.5/src/main/java/com/github/jklasd/test/lazyplugn/spring/configprop/BinderHandler.java
+++
@@ -1,7 +0,0 @@
-package com.github.jklasd.test.lazyplugn.spring.configprop;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-public interface BinderHandler {
-	void postProcess(Object obj, ConfigurationProperties annotation);
-}
