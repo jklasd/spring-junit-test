2a47f16f2b4fddc53095c43b14f5f15c6dfb4161
那一天 <jubin.zhang@welab-inc.com>
2022-05-11 18:28:59 +0800

    1.2.2-resource List 处理

--- spring-junit-test-spring-boot-n.n/src/main/java/com/github/jklasd/test/core/common/fieldann/ResourceHandler.java
+++ spring-junit-test-spring-boot-n.n/src/main/java/com/github/jklasd/test/core/common/fieldann/ResourceHandler.java
@@ -2,9 +2,13 @@ package com.github.jklasd.test.core.common.fieldann;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.List;
 
 import javax.annotation.Resource;
 
+import com.github.jklasd.test.common.JunitClassLoader;
 import com.github.jklasd.test.common.component.FieldAnnComponent;
 import com.github.jklasd.test.common.interf.handler.FieldHandler;
 import com.github.jklasd.test.common.model.FieldDef;
@@ -19,7 +23,25 @@ public class ResourceHandler  implements FieldHandler{
 		Object obj = def.getTagObj();
 		Resource c = (Resource) ann;
 		if (c != null) {
-			FieldAnnComponent.setObj(attr, obj, LazyBean.getInstance().buildProxy(attr.getType(),c.name()));
+			if(attr.getType() == List.class) {
+				ParameterizedType t = (ParameterizedType) attr.getGenericType();
+				Type[] item = t.getActualTypeArguments();
+				if(item.length == 1) {
+					//处理一个集合注入
+					try {
+						Class<?> itemC = JunitClassLoader.getInstance().junitloadClass(item[0].getTypeName());
+						List<?> list = LazyBean.findListBean(itemC);
+						FieldAnnComponent.setObj(attr, obj, list);
+						log.info("{}注入resource集合=>{},{}个对象",obj.getClass(),attr.getName(),list.size());
+					} catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					}
+				}else {
+					log.info("其他特殊情况");
+				}
+			}else {
+				FieldAnnComponent.setObj(attr, obj, LazyBean.getInstance().buildProxy(attr.getType(),c.name()));
+			}
 		}
 	}
 	public String getType() {
